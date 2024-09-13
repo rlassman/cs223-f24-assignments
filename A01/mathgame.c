9ce9646825944cf836adf3d0e0c232a9cb361582
Rebecca Lily Lassman <rlassman@goldengate.cs.brynmawr.edu>
2024-09-13 14:04:28 -0400

    Final version

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,4 +1,4 @@
-/***************************************************
+ /***************************************************
  * mathgame.c
  * Author: Rebecca Lassman
  * Implements a math game
@@ -9,15 +9,15 @@
 
 int main() {
 
-  int response = 0;
+  int rounds = 0;
   int correct = 0;
   int max = 9;
   int min = 1;
 
   printf("Welcome to Math Game!\nHow many rounds do you want to play? ");
-  scanf("%d", &response);
+  scanf("%d", &rounds);
   
-  for (int i=0; i<response; i++) {
+  for (int i=0; i<rounds; i++) {
 	int num1 = rand() % (max - min + 1) + min;
 	int num2 = rand() % (max - min + 1) + min;
 	int answer = 0;
@@ -30,8 +30,5 @@ int main() {
 		printf("Incorrect :(\n");
   }
 
-  printf("You answered %d/%d correctly.\n", correct, response);
-
-  
+  printf("You answered %d/%d correctly.\n", correct, rounds);
 }
-
