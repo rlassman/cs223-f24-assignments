d4333d7ff4011f1edfa850f77eedc85e5081a6fd
Rebecca Lily Lassman <rlassman@goldengate.cs.brynmawr.edu>
2024-09-19 21:04:46 -0400

    assignment 3 complete

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,70 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Rebecca Lassman
+ * Date: 9/20/24
+ * Description: Prints characteristics for a list of songs
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct song {
+	char title[128];
+  	char artist[128];
+  	int durMin;
+	int durSec;
+  	float dance;
+	float energy;
+	float tempo;
+	float valence;
+} song;
+
+int printSongs(song* songArr, int n) {
+	for(int i=0; i<n; i++) {
+        	printf("\n%d) %-30s artist: %-20s duration: %d:%02d  danceability: %.2f energy:  %.2f tempo:  %.2f valence:  %.2f", 
+                       i, songArr[i].title, songArr[i].artist, songArr[i].durMin, songArr[i].durSec, songArr[i].dance, songArr[i].energy, 
+		       songArr[i].tempo, songArr[i].valence);
+  	}
+  	printf("\n\n========================\n"); 
+	return 0;
+}
 
 int main() {
-  return 0;
+	FILE *infile ;
+	infile = fopen("songlist.csv", "r");
+	if (infile == NULL) {
+		printf("Error, unable to open file");
+		exit(1);
+	}
+ 	
+	char buffer[100];
+	fgets(buffer, 100, infile); 
+	char *tok = strtok(buffer, ",");
+	int size = atoi(tok);
+
+	song* songs = malloc(sizeof(song) * size);
+	fgets(buffer, 100, infile);
+	for (int i = 0; i < size; i++) {
+		fgets(buffer, 100, infile);
+		tok = strtok(buffer, ",");
+		strcpy(songs[i].title, tok);
+		tok = strtok(NULL, ",");
+		strcpy(songs[i].artist, tok);
+		tok = strtok(NULL, ",");
+		int dur = atoi(tok)/1000;
+		songs[i].durMin = dur/60;
+		songs[i].durSec = dur % 60;
+		tok = strtok(NULL, ",");
+		songs[i].dance = atof(tok);
+		tok = strtok(NULL, ",");
+                songs[i].energy = atof(tok);
+		tok = strtok(NULL, ",");
+                songs[i].tempo = atof(tok);
+		tok = strtok(NULL, ",");
+                songs[i].valence = atof(tok);
+	}
+
+	printSongs(songs, size);	
+	free(songs);
+	fclose(infile);
+	return 0;
 }
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,37 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Rebecca Lassman
+ * Date: 9/20/24
+ * Description: Converts a phrase to entish
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-  return 0;
+	int pause = 0;
+	char phrase[32];
+	char *entish;
+	int entIn = 0;
+	int length;
+
+	printf("Pause length: ");
+	scanf("%d", &pause);
+	printf("Text: ");
+	scanf(" %[^\n]%*c", phrase);
+	length = strlen(phrase);
+	entish = malloc(sizeof(char) * (length * pause + length + 1));
+
+	for (int i = 0; i < strlen(phrase); i++) {
+		entish[entIn] = phrase[i];
+		entIn++;
+		for (int j = 0; j < pause; j++) {
+			entish[entIn] = '.';
+			entIn ++;
+		}	
+	}
+	entish[entIn] = '\0';
+	printf("%s\n", entish);
+
+	free(entish);
+	return 0;
 }
